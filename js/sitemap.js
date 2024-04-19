bf882b483f5d7004351475f4fd83ad3278603087
AkanshaFernando <akanshafdo@gmail.com>
2024-04-19 20:30:34 +0530

    session storage updated

--- js/sitemap.js
+++ js/sitemap.js
@@ -6,14 +6,14 @@ const rectangles = document.querySelectorAll('rect');
 
 // Function to set the selected color in local storage
 function setSelectedColor(color) {
-    localStorage.setItem('selectedColor', color);
+    sessionStorage.setItem('selectedColor', color);
 }
 
 // Function to get the selected color from local storage
 function getSelectedColor() {
 // Check if there's a selected color in local storage
 // If not, return the default color
-    return localStorage.getItem('selectedColor') || 'dodgerblue';
+    return sessionStorage.getItem('selectedColor') || 'dodgerblue';
 }
 
 // Function to update the fill color of rectangles
